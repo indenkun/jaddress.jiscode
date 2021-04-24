7fe7d303f43f3199063bc50c24533157a9a91dee
indenkun <kobamao.jp@gmail.com>
2021-04-24 21:41:17 +0900

    small fix of manual documents.

--- R/jaddress_jiscode.R
+++ R/jaddress_jiscode.R
@@ -12,8 +12,8 @@
 #' @return a charactor.
 #'
 #' @details
-#' The data for the organization code comes from the data published by the Ministry of Internal Affairs and Communications.
-#' Source: Ministry of Internal Affairs and Communications website, National Local Government Code (https://www.soumu.go.jp/denshijiti/code.html) "Prefectural Code and Municipal Code" (updated on May 1, 2019)
+#' The data for the JIS code from the data published by the Ministry of Internal Affairs and Communications.
+#' Source: Ministry of Internal Affairs and Communications website, National Local Government Code (https://www.soumu.go.jp/denshijiti/code.html) "Prefectural Code and Municipal Code" (updated on May 1, 2019).
 #' The copyright of the data published by the Ministry of Internal Affairs and Communications (MIC) on its website belongs to the Ministry of Internal Affairs and Communications (MIC), but processing and redistribution are permitted within a certain range.
 #' For details, please refer to the web page of the Ministry of Internal Affairs and Communications.
 #'
