d917f670009884754c654ba721913117a00c1742
indenkun <kobamao.jp@gmail.com>
2021-04-24 21:41:55 +0900

    add a zzz.R and package start up message.

---
+++ R/zzz.R
@@ -0,0 +1,3 @@
+.onAttach <- function(...){
+  packageStartupMessage("The data for the JIS code from the data published by the Ministry of Internal Affairs and Communications.\nSource: Ministry of Internal Affairs and Communications website, National Local Government Code (https://www.soumu.go.jp/denshijiti/code.html) 'Prefectural Code and Municipal Code' (updated on May 1, 2019).")
+}
